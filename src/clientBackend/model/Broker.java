743484ad32341f8902bd95e96309e2cc8be93409
Kevin J Reece <kevinjreece@gmail.com>
2015-01-22 09:20:43 -0700

    started javadocs for Broker.java

--- src/clientBackend/model/Broker.java
+++ src/clientBackend/model/Broker.java
@@ -1,8 +1,32 @@
 package clientBackend.model;
 
+import java.util.List;
+import java.util.Map;
+
 /**
- * Responsible for the transfer of cards between and among the bank and players' hands 
+ * Responsible for the transfer of cards between and among the bank and players' holdings 
  * */
 public class Broker {
+	private Map<PlayerNumber, PlayerHoldings> holdings;
+	
+	/** TODO
+	 * Initiates a transfer of cards between or among the bank and players' holdings
+	 * @param	invoices	a list of invoices to process
+	 * @return				indicates success of invoices
+	 * */
+	public boolean processInvoices(List<ResourceInvoice> invoices) {
+		return false;
+	}
+	
+	/** TODO
+	 * Transfers the correct cards from a player's holdings to the bank
+	 * for the given property
+	 * @param playerNum
+	 * @param propertyType
+	 * @return
+	 */
+	public boolean purchase(PlayerNumber playerNum, Property propertyType) {
+		return false;
+	}
 
 }
