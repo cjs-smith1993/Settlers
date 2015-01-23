0e305c6f6a4058a462803dfd6624567be73c864b
Alex Thomas <alexcthomas18@gmail.com>
2015-01-22 20:47:36 -0700

    I am starting road, dwelling, settlement and cities.

---
+++ src/clientBackend/model/Dwelling.java
@@ -0,0 +1,18 @@
+package clientBackend.model;
+
+import shared.definitions.CatanColor;
+import shared.definitions.PlayerNumber;
+import shared.locations.VertexLocation;
+
+/**
+ * This is the dwelling class that will hold similarities between settlements and cities.
+ * @author alexthomas
+ *
+ */
+public class Dwelling {
+	VertexLocation myLocation;
+	PlayerNumber myOwner;
+	CatanColor myColor;
+	
+
+}
---
+++ src/clientBackend/model/Road.java
@@ -0,0 +1,10 @@
+package clientBackend.model;
+
+import shared.definitions.PlayerNumber;
+import shared.locations.EdgeLocation;
+
+public class Road {
+	EdgeLocation myLocation;
+	PlayerNumber myOwner;
+
+}
