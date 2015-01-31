6702680a02f039fd15f67c2cb2d14e51e88e3f30
Connor Smith <cjs.smith1993@gmail.com>
2015-01-30 17:27:02 -0700

    modified enums

--- src/shared/definitions/PlayerNumber.java
+++ src/shared/definitions/PlayerNumber.java
@@ -1,5 +1,11 @@
 package shared.definitions;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum PlayerNumber {
-	BANK, ONE, TWO, THREE, FOUR
+	@SerializedName("-1")BANK,
+	@SerializedName("0")ONE,
+	@SerializedName("1")TWO,
+	@SerializedName("2")THREE,
+	@SerializedName("3")FOUR
 }
