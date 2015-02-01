cd0bf9461f78c445eeaa1c46cd71d61439d1e8d7
Connor Smith <cjs.smith1993@gmail.com>
2015-02-01 15:09:03 -0700

    added constructor/getters/setters for Road

--- src/clientBackend/model/Road.java
+++ src/clientBackend/model/Road.java
@@ -10,4 +10,21 @@ import shared.locations.*;
 public class Road {
 	private EdgeLocation location;
 	private PlayerNumber owner;
+	
+	public Road(PlayerNumber owner) {
+		this.location = null;
+		this.owner = owner;
+	}
+	
+	public EdgeLocation getLocation() {
+		return this.location;
+	}
+	
+	public void setLocation(EdgeLocation location) {
+		this.location = location;
+	}
+	
+	public PlayerNumber getOwner() {
+		return this.owner;
+	}
 }
