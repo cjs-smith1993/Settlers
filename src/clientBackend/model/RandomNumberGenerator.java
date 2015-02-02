3559b5a620ff11cf9de6eb6e6de570a19e943371
Connor Smith <cjs.smith1993@gmail.com>
2015-02-01 18:04:06 -0700

    possible implementation of RNG and Dice

--- src/clientBackend/model/RandomNumberGenerator.java
+++ src/clientBackend/model/RandomNumberGenerator.java
@@ -1,18 +1,37 @@
 package clientBackend.model;
 
+import java.util.Random;
+
 /**
  * Generates a random number
  */
 public class RandomNumberGenerator {
-	private int seed;
-	
+	private Random rng;
+
+	public RandomNumberGenerator() {
+		this.rng = new Random();
+	}
+
+	public RandomNumberGenerator(long seed) {
+		this.rng = new Random(seed);
+	}
+
+	public void reSeed() {
+		this.rng = new Random();
+	}
+
+	public void reSeed(long seed) {
+		this.rng.setSeed(seed);
+	}
+
 	/**
 	 * Generates a random integer between the two bounds (inclusive)
+	 *
 	 * @param lowerBound
 	 * @param upperBound
 	 * @return the generated number
 	 */
-	static public int generate(int lowerBound, int upperBound) {
-		return 0;
+	public int generate(int lowerBound, int upperBound) {
+		return this.rng.nextInt(upperBound - lowerBound + 1) + lowerBound;
 	}
 }
