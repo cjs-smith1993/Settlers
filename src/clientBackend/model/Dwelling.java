4429bf4d97ab9f9e6ec58a0865cf0ea84bca204f
Connor Smith <cjs.smith1993@gmail.com>
2015-02-03 14:04:05 -0700

    took out unused code for dwellings

--- src/clientBackend/model/Dwelling.java
+++ src/clientBackend/model/Dwelling.java
@@ -1,7 +1,5 @@
 package clientBackend.model;
 
-import java.util.Collection;
-
 import shared.definitions.*;
 import shared.locations.*;
 
@@ -9,28 +7,14 @@ import shared.locations.*;
  * Represents the abstract concept of a dwelling. A dwelling adds a number of
  * victory points to the owner, depending on the dwelling's type. A dwelling is
  * located at a vertex and generates resources for the owner of the dwelling,
- * based off the types of the dwelling's adjacent tiles. The number of
- * resources a dwelling generates from a connected tile is equal to the number
- * of victory points for the dwelling
+ * based off the types of the dwelling's adjacent tiles. The number of resources
+ * a dwelling generates from a connected tile is equal to the number of victory
+ * points for the dwelling
  */
 public abstract class Dwelling {
 	private VertexLocation location;
 	protected int victoryPoints;
 	protected PlayerNumber owner;
-	
-	private Collection<Tile> getAdjacentTiles() {
-		return null;
-	}
-	
-	/**
-	 * Generates and returns a collection of invoices. One invoice will be
-	 * generated for each connected tile. Invoices with the same resource type
-	 * will be combined into a single invoice
-	 * @return a collection of invoices generated from each connected tile
-	 */
-	public Collection<ResourceInvoice> harvestResources() {
-		return null;
-	}
 
 	public VertexLocation getLocation() {
 		return this.location;
