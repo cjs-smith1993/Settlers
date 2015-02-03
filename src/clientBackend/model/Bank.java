cfd02c927504e88ad2a973344f923bc369c9a818
Alex Thomas <alexcthomas18@gmail.com>
2015-02-02 17:01:19 -0700

    added Hand interface and made it working

--- src/clientBackend/model/Bank.java
+++ src/clientBackend/model/Bank.java
@@ -9,7 +9,7 @@ import shared.definitions.*;
  * Represents the bank, which manages the collections of available resource and
  * development cards, as well as played development cards
  */
-public class Bank {
+public class Bank implements Hand{
 	private Map<ResourceType, Collection<ResourceCard>> resourceCards;
 	private Map<DevCardType, Collection<DevelopmentCard>> developmentCards;
 	private Collection<DevelopmentCard> playedCards;
@@ -23,22 +23,22 @@ public class Bank {
 	}
 	
 	/**
-	 * Returns the number of available development cards in the bank
-	 * @return the number of available development cards
-	 */
-	public int getDevelopmentCardCound() {
-		return 0;
-	}
-	
-	/**
 	 * Returns a resource card of the desired type from the corresponding deck
 	 * @param player
 	 * @param type
 	 */
-	public void drawResourceCard(PlayerNumber player, ResourceType type) {
-		
+	@Override
+	public Collection<ResourceCard> removeResourceCard(ResourceType type,
+			int count) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
+	@Override
+	public int getDevelopmentCardCount(DevCardType type) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	/**
 	 * Returns the number of available resource cards of the desired type in
 	 * the bank
@@ -47,5 +47,21 @@ public class Bank {
 	 */
 	public int getResourceCardCount(ResourceType type) {
 		return 0;
+	}
+
+	@Override
+	public boolean addResourceCardCollection(ResourceType type,
+			Collection<ResourceCard> newCards) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+
+	@Override
+	public boolean addDevelopmentCardCollection(DevCardType type,
+			Collection<DevelopmentCard> newCards) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
