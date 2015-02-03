a132e26009efc46b9ccb5aef6855ce942dc70e6d
kbpontius <kbpontius@gmail.com>
2015-02-02 19:31:16 -0700

    Added all methods necessary to the Scoreboard class. Also, fixed a problem in the "TransportTurnTracker" class where it said "LargestPlayer" not "LargestArmy".

--- src/clientBackend/model/Scoreboard.java
+++ src/clientBackend/model/Scoreboard.java
@@ -1,8 +1,14 @@
 package clientBackend.model;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import shared.definitions.*;
+import clientBackend.transport.TransportMap;
+import clientBackend.transport.TransportPlayer;
+import clientBackend.transport.TransportRoad;
+import clientBackend.transport.TransportTurnTracker;
 
 /**
  * The scoreboard manages the victory points of all players in the game. Each
@@ -11,26 +17,108 @@ import shared.definitions.*;
  * or playing a development card), the scoreboard will update
  */
 public class Scoreboard {
-	private Map<PlayerNumber, Integer> points;
 	private PlayerNumber largestArmy;
 	private PlayerNumber longestRoad;
+	private Map<PlayerNumber, Integer> points;
 	private Map<PlayerNumber, Integer> activeKnights;
 	private Map<PlayerNumber, Integer> builtRoads;
+	
+	public Scoreboard(TransportMap map, List<TransportPlayer> player, TransportTurnTracker turnTracker) {
+		initializeMap(points);
+		initializeMap(activeKnights);
+		initializeMap(builtRoads);
+		
+		largestArmy = turnTracker.largestArmy;
+		longestRoad = turnTracker.longestRoad;
+		
+		countRoads(map);
+		countKnights(turnTracker);
+		countPoints(player);
+	}
+	
+	public Scoreboard() {
+		initializeMap(points);
+		initializeMap(activeKnights);
+		initializeMap(builtRoads);
+		
+		largestArmy = PlayerNumber.BANK;
+		longestRoad = PlayerNumber.BANK;
+	}
+	
+	private void initializeMap(Map<PlayerNumber, Integer> map) {
+		map = new HashMap<PlayerNumber, Integer>();
+		
+		for (int i = 0; i < 5; i++) {
+			map.put(PlayerNumber.values()[i], 0);
+		}
+	}
+	
+	private void countRoads(TransportMap map) {
+		List<TransportRoad> roads = map.roads;
+		
+		for (int i = 0; i < roads.size(); i++) {
+			updatePlayerMaps(builtRoads, roads.get(i).owner, 1);
+		}
+	}
+	
+	private void countKnights(TransportTurnTracker turnTracker) {
+		// TODO: Needs to find a way to count the number of active knight cards.
+	}
+	
+	private void countPoints(List<TransportPlayer> player) {
+		for (int i = 0; i < player.size(); i++) {
+			points.put(player.get(i).playerIndex, player.get(i).victoryPoints);
+		}
+	}
+	
+	private void updatePlayerMaps(Map<PlayerNumber, Integer> playerMap, PlayerNumber player, int difference) {
+		int currentValue = playerMap.get(player);
+		currentValue += difference;
+		playerMap.put(player, currentValue);
+	}
 	
 	/**
 	 * Adds one victory point to the player who built the dwelling
 	 * @param player the player who built the dwelling
 	 */
 	public void dwellingBuilt(PlayerNumber player) {
-		
+		updatePlayerMaps(points, player, 1);
 	}
 	
 	/**
-	 * Recalculates the owner of the longest road and adjusts victory points,
-	 * if necessary
+	 * Adds a road to a player, then recalculates the owner of the longest road and adjusts victory points,
+	 * if necessary.
 	 */
-	public void roadBuilt() {
+	public void roadBuilt(PlayerNumber player) {
+		int playerRoads = builtRoads.get(player);
+		playerRoads++;
+		builtRoads.put(player, playerRoads);
 		
+		calculateSpecialCardHolder(builtRoads, longestRoad);
+	}
+	
+	/**
+	 * This method calculates the PlayerNumber with the largest Integer as a value in the map.
+	 * This method works with LongestRoad and LargestArmy.
+	 * @param specialCard
+	 * @param currentHolder
+	 */
+	private void calculateSpecialCardHolder(Map<PlayerNumber, Integer> specialCard, PlayerNumber currentHolder) {
+		PlayerNumber currentWinner = currentHolder;
+		PlayerNumber oldWinner = currentWinner;
+		
+		for (Map.Entry<PlayerNumber, Integer> cardCount : specialCard.entrySet()) {
+			if (cardCount.getValue() >= 5) {
+				if (cardCount.getValue() > specialCard.get(currentWinner)) {
+					currentWinner = cardCount.getKey();
+				}
+			}
+		}
+		
+		if (oldWinner != currentWinner) {
+			updatePlayerMaps(points, oldWinner, -2);
+			updatePlayerMaps(points, currentWinner, 2);
+		}
 	}
 	
 	/**
@@ -42,7 +130,21 @@ public class Scoreboard {
 	 * @param type
 	 */
 	public void devCardPlayed(PlayerNumber player, DevCardType type) {
-		
+		switch (type) {
+		case MONUMENT:
+			updatePlayerMaps(points, player, 1);
+			break;
+		case SOLDIER:
+			updatePlayerMaps(activeKnights, player, 1);
+			calculateSpecialCardHolder(activeKnights, largestArmy);
+			break;
+		case ROAD_BUILD:
+			updatePlayerMaps(builtRoads, player, 1);
+			calculateSpecialCardHolder(builtRoads, longestRoad);
+			break;
+		default:
+			break;
+		}
 	}
 	
 	/**
@@ -51,6 +153,6 @@ public class Scoreboard {
 	 * @return the number of victory points for the player
 	 */
 	public int getPoints(PlayerNumber player) {
-		return 0;
+		return points.get(player);
 	}
 }
