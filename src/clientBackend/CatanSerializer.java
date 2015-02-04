d2a4be6fb161a954e1b33e204a956e49ad4d0205
kbpontius <kbpontius@gmail.com>
2015-02-03 21:57:39 -0700

    Added implementation to the model.

--- src/clientBackend/CatanSerializer.java
+++ src/clientBackend/CatanSerializer.java
@@ -1,10 +1,15 @@
 package clientBackend;
 
+import clientBackend.transport.TransportModel;
+
+import com.google.gson.Gson;
+
 /**
  * Serializes and deserializes between the model's internal representation and
  * JSON used for communicating with the server
  */
 public class CatanSerializer {
+	final Gson gson = new Gson();
 	
 	/**
 	 * Converts the model's internal representation into a JSON string using the
@@ -17,28 +22,38 @@ public class CatanSerializer {
 	
 	/**
 	 * Converts the given JSON string into the model's internal representation
-	 * using the Gson open-source library
-	 * @param gson a JSON representation of the internal model
+	 * using the Gson open-source library.
+	 * 
+	 * @param gsonString a JSON representation of the internal model
 	 */
-	public void deserializeModel(String gson) {
+	public void deserializeModel(String gsonString) {
+		TransportModel transportModel = gson.fromJson(gsonString, TransportModel.class);
 		
+		System.out.println(transportModel);
+		
+		// TODO: Distribute model information to appropriate class?
 	}
 	
 	/**
 	 * Converts any Catan object into a json string
+	 * 
 	 * @param o the object to be serialized
 	 * @return the JSON representation of the object
 	 * */
 	public String serializeObject(Object o) {
-		return null;
+		String serializedObject = gson.toJson(o);
+		
+		return serializedObject;
 	}
 	
 	/**
 	 * Converts any JSON string into a Catan object
-	 * @param gson a JSON representation of a Catan object
+	 * @param gsonString a JSON representation of a Catan object
 	 * @return the JSON representation of the object
 	 * */
-	public Object deserializeObject(String gson) {
-		return null;
+	public Object deserializeObject(String gsonString) {
+		Object deserializedObject = gson.fromJson(gsonString, Object.class);
+		
+		return deserializedObject;
 	}
 }
