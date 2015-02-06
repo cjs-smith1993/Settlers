3874a689420dabc9022e6c2646d0209fc0085725
Connor Smith <cjs.smith1993@gmail.com>
2015-02-05 22:34:41 -0700

    added equals and toString methods to ResourceInvoice

--- src/clientBackend/model/ResourceInvoice.java
+++ src/clientBackend/model/ResourceInvoice.java
@@ -8,29 +8,29 @@ import shared.definitions.ResourceType;
  * one resource between one player and either another player or the bank
  */
 public class ResourceInvoice {
-	
-	
+
 	/**
-	 * The type of the resource 
+	 * The type of the resource
 	 */
 	public ResourceType resource;
-	
+
 	/**
 	 * The number of resources (greater than zero)
 	 */
 	public int count;
-	
+
 	/**
 	 * The player from which the resource is being withdrawn, or the bank
 	 */
 	public PlayerNumber sourcePlayer;
-	
+
 	/**
 	 * The player that is receiving the resource, or the bank
 	 */
 	public PlayerNumber destinationPlayer;
-	
-	public ResourceInvoice(ResourceType type, int count, PlayerNumber sourcePlayer, PlayerNumber destinationPlayer)
+
+	public ResourceInvoice(ResourceType type, int count, PlayerNumber sourcePlayer,
+			PlayerNumber destinationPlayer)
 	{
 		this.resource = type;
 		this.count = count;
@@ -39,7 +39,7 @@ public class ResourceInvoice {
 	}
 
 	public ResourceType getResource() {
-		return resource;
+		return this.resource;
 	}
 
 	public void setResource(ResourceType resource) {
@@ -47,7 +47,7 @@ public class ResourceInvoice {
 	}
 
 	public int getCount() {
-		return count;
+		return this.count;
 	}
 
 	public void setCount(int count) {
@@ -55,7 +55,7 @@ public class ResourceInvoice {
 	}
 
 	public PlayerNumber getSourcePlayer() {
-		return sourcePlayer;
+		return this.sourcePlayer;
 	}
 
 	public void setSourcePlayer(PlayerNumber sourcePlayer) {
@@ -63,11 +63,45 @@ public class ResourceInvoice {
 	}
 
 	public PlayerNumber getDestinationPlayer() {
-		return destinationPlayer;
+		return this.destinationPlayer;
 	}
 
 	public void setDestinationPlayer(PlayerNumber destinationPlayer) {
 		this.destinationPlayer = destinationPlayer;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		ResourceInvoice other = (ResourceInvoice) obj;
+		if (this.count != other.count) {
+			return false;
+		}
+		if (this.destinationPlayer != other.destinationPlayer) {
+			return false;
+		}
+		if (this.resource != other.resource) {
+			return false;
+		}
+		if (this.sourcePlayer != other.sourcePlayer) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ResourceInvoice [resource=" + this.resource + ", count=" + this.count
+				+ ", sourcePlayer=" + this.sourcePlayer + ", destinationPlayer="
+				+ this.destinationPlayer + "]";
+	}
+
 }
