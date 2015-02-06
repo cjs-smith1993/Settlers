cc7552fcd0990016584b43f351de3d0c612f0103
Connor Smith <cjs.smith1993@gmail.com>
2015-02-06 14:49:16 -0700

    made CatanSerializer a singleton

--- src/clientBackend/CatanSerializer.java
+++ src/clientBackend/CatanSerializer.java
@@ -12,54 +12,72 @@ import com.google.gson.Gson;
  */
 public class CatanSerializer {
 	final Gson gson = new Gson();
+	private static CatanSerializer serializer;
 	private Facade facade;
-	
-	public CatanSerializer(Facade facade) {
+
+	public static CatanSerializer getInstance() {
+		if (serializer != null) {
+			serializer = new CatanSerializer();
+		}
+		return serializer;
+	}
+
+	private CatanSerializer() {
+
+	}
+
+	public void setFacade(Facade facade) {
 		this.facade = facade;
 	}
-	
-//	----> THIS COMMENTED OUT AS WE LIKELY WON'T NEED THIS FUNCTIONALITY CLIENT-SIDE.
-//	/**
-//	 * Converts the model's internal representation into a JSON string using the
-//	 * Gson open-source library
-//	 * @return a JSON representation of the internal model
-//	 */
-//	public String serializeModel() {
-//		return null;
-//	}
-	
+
+	// ----> THIS COMMENTED OUT AS WE LIKELY WON'T NEED THIS FUNCTIONALITY
+	// CLIENT-SIDE.
+	// /**
+	// * Converts the model's internal representation into a JSON string using
+	// the
+	// * Gson open-source library
+	// * @return a JSON representation of the internal model
+	// */
+	// public String serializeModel() {
+	// return null;
+	// }
+
 	/**
 	 * Converts the given JSON string into the model's internal representation
 	 * using the Gson open-source library.
-	 * 
-	 * @param gsonString a JSON representation of the internal model
-	 * @throws CatanException 
+	 *
+	 * @param gsonString
+	 *            a JSON representation of the internal model
+	 * @throws CatanException
 	 */
 	public void deserializeModel(String gsonString) throws CatanException {
-		TransportModel transportModel = gson.fromJson(gsonString, TransportModel.class);
-		
-		facade.initializeModel(transportModel);
+		TransportModel transportModel = this.gson.fromJson(gsonString, TransportModel.class);
+
+		this.facade.initializeModel(transportModel);
 	}
-	
+
 	/**
 	 * Converts any Catan object into a json string
-	 * 
-	 * @param o the object to be serialized
+	 *
+	 * @param o
+	 *            the object to be serialized
 	 * @return the JSON representation of the object
 	 * */
 	public String serializeObject(Object o) {
-		String serializedObject = gson.toJson(o);
-		
+		String serializedObject = this.gson.toJson(o);
+
 		return serializedObject;
 	}
-	
+
 	/**
 	 * Converts any JSON string into a Catan object
-	 * @param gsonString a JSON representation of a Catan object
+	 *
+	 * @param gsonString
+	 *            a JSON representation of a Catan object
 	 * @return the JSON representation of the object
 	 * */
 	public Object deserializeObject(String gsonString) {
-		Object deserializedObject = gson.fromJson(gsonString, Object.class);
+		Object deserializedObject = this.gson.fromJson(gsonString, Object.class);
 
 		return deserializedObject;
 	}
