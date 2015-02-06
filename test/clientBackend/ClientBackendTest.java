1f797e8feeefcb920af206801ca341535fcc186a
Connor Smith <cjs.smith1993@gmail.com>
2015-02-06 03:14:16 -0700

    got ant testing working

--- test/clientBackend/ClientBackendTest.java
+++ test/clientBackend/ClientBackendTest.java
@@ -1,9 +1,8 @@
-package model;
+package clientBackend;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
 
 public class ClientBackendTest {
 
@@ -13,13 +12,13 @@ public class ClientBackendTest {
 	}
 
 	public static void main(String[] args) {
-		
+
 		String[] testClasses = new String[] {
 				"clientBackend.model.BoardTest",
 				"clientBackend.model.PlayerHoldingsTest"
 		};
-		
+
 		org.junit.runner.JUnitCore.main(testClasses);
 	}
-	
+
 }
