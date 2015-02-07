dd72631756f5be35eb1c9a9b9fd73ee715be8913
Connor Smith <cjs.smith1993@gmail.com>
2015-02-07 00:09:31 -0700

    fixes

--- test/clientBackend/model/FacadeTest.java
+++ test/clientBackend/model/FacadeTest.java
@@ -29,68 +29,67 @@ public class FacadeTest {
 
 	@Before
 	public void setUp() throws Exception {
-		facade = new Facade();
-		model = new TransportModel();
-		player = new TransportPlayer();
-		
-		model.deck = new TransportDeck();
-		model.bank = new TransportBank();
-		model.players = new ArrayList<TransportPlayer>();
-		model.map = new TransportMap();
-		model.turnTracker = new TransportTurnTracker();
-		model.chat = new TransportChat();
-		model.chat.lines = new ArrayList<TransportLine>();
-		model.log = new TransportLog();
-		model.log.lines = new ArrayList<TransportLine>();
-		
-		model.turnTracker.status = Status.PLAYING;
-		model.turnTracker.currentTurn = PlayerNumber.ONE;
-		
-		model.deck.yearOfPlenty = 2;
-		model.deck.monopoly = 2;
-		model.deck.soldier = 14;
-		model.deck.roadBuilding = 2;
-		model.deck.monument = 5;
-		
-		model.bank.brick = 23;
-		model.bank.ore = 21;
-		model.bank.sheep = 23;
-		model.bank.wheat = 21;
-		model.bank.wood = 23;
-		
-		player.resources = new TransportResources();
-		player.oldDevCards = new TransportOldDevCards();
-		player.newDevCards = new TransportNewDevCards();
-		
-		
-		player.resources.brick = 2;
-		player.resources.ore = 4;
-		player.resources.sheep = 2;
-		player.resources.wheat = 4;
-		player.resources.wood = 2;
-		
-		player.oldDevCards.yearOfPlenty = 1;
-		player.oldDevCards.monopoly = 1;
-		player.oldDevCards.soldier = 1;
-		player.oldDevCards.roadBuilding = 1;
-		player.oldDevCards.monument = 1;
-		
-		player.roads = 15;
-		player.settlements = 5;
-		player.cities = 4;
-		player.playerIndex = PlayerNumber.ONE;
-		
-		model.players.add(player);
-		
-		model.map.hexes = new ArrayList<TransportHex>();
-		model.map.roads = new ArrayList<TransportRoad>();
-		model.map.cities = new ArrayList<TransportCity>();
-		model.map.settlements = new ArrayList<TransportSettlement>();
-		model.map.ports = new ArrayList<TransportPort>();
-		model.map.robber = new TransportRobber();
-		
+		this.facade = new Facade();
+		this.model = new TransportModel();
+		this.player = new TransportPlayer();
+
+		this.model.deck = new TransportDeck();
+		this.model.bank = new TransportBank();
+		this.model.players = new TransportPlayer[1];
+		this.model.map = new TransportMap();
+		this.model.turnTracker = new TransportTurnTracker();
+		this.model.chat = new TransportChat();
+		this.model.chat.lines = new ArrayList<TransportLine>();
+		this.model.log = new TransportLog();
+		this.model.log.lines = new ArrayList<TransportLine>();
+
+		this.model.turnTracker.status = Status.PLAYING;
+		this.model.turnTracker.currentTurn = PlayerNumber.ONE;
+
+		this.model.deck.yearOfPlenty = 2;
+		this.model.deck.monopoly = 2;
+		this.model.deck.soldier = 14;
+		this.model.deck.roadBuilding = 2;
+		this.model.deck.monument = 5;
+
+		this.model.bank.brick = 23;
+		this.model.bank.ore = 21;
+		this.model.bank.sheep = 23;
+		this.model.bank.wheat = 21;
+		this.model.bank.wood = 23;
+
+		this.player.resources = new TransportResources();
+		this.player.oldDevCards = new TransportOldDevCards();
+		this.player.newDevCards = new TransportNewDevCards();
+
+		this.player.resources.brick = 2;
+		this.player.resources.ore = 4;
+		this.player.resources.sheep = 2;
+		this.player.resources.wheat = 4;
+		this.player.resources.wood = 2;
+
+		this.player.oldDevCards.yearOfPlenty = 1;
+		this.player.oldDevCards.monopoly = 1;
+		this.player.oldDevCards.soldier = 1;
+		this.player.oldDevCards.roadBuilding = 1;
+		this.player.oldDevCards.monument = 1;
+
+		this.player.roads = 15;
+		this.player.settlements = 5;
+		this.player.cities = 4;
+		this.player.playerIndex = PlayerNumber.ONE;
+
+		this.model.players[0] = this.player;
+
+		this.model.map.hexes = new ArrayList<TransportHex>();
+		this.model.map.roads = new ArrayList<TransportRoad>();
+		this.model.map.cities = new ArrayList<TransportCity>();
+		this.model.map.settlements = new ArrayList<TransportSettlement>();
+		this.model.map.ports = new ArrayList<TransportPort>();
+		this.model.map.robber = new TransportRobber();
+
 		try {
-			facade.initializeModel(model);
+			this.facade.initializeModel(this.model);
 		} catch (CatanException e) {
 			e.printStackTrace();
 			fail();
@@ -100,51 +99,54 @@ public class FacadeTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	public void testCanDiscardCards() throws CatanException {
-		
-		player.resources.brick = 8;
-		player.resources.ore = 0;
-		player.resources.sheep = 0;
-		player.resources.wheat = 0;
-		player.resources.wood = 0;
-		model.players = new ArrayList<TransportPlayer>();
-		model.players.add(player);
-		model.turnTracker.status = Status.DISCARDING;
-		facade.initializeModel(model);
-		/* Should work because the status is discarding and the player has more
-		 * than 7 cards */
-		assertTrue(facade.canDiscardCards(PlayerNumber.ONE));
-		
-		player.resources.brick = 7;
-		model.players = new ArrayList<TransportPlayer>();
-		model.players.add(player);
-		facade.initializeModel(model);
-		/* Should fail because the client player only has 7 resource cards*/
-		assertFalse(facade.canDiscardCards(PlayerNumber.ONE));
-		
-		model.turnTracker.status = Status.ROLLING;
-		player.resources.brick = 8;
-		model.players = new ArrayList<TransportPlayer>();
-		model.players.add(player);
-		facade.initializeModel(model);
+
+		this.player.resources.brick = 8;
+		this.player.resources.ore = 0;
+		this.player.resources.sheep = 0;
+		this.player.resources.wheat = 0;
+		this.player.resources.wood = 0;
+		this.model.players = new TransportPlayer[1];
+		this.model.players[0] = this.player;
+		this.model.turnTracker.status = Status.DISCARDING;
+		this.facade.initializeModel(this.model);
+		/*
+		 * Should work because the status is discarding and the player has more
+		 * than 7 cards
+		 */
+		assertTrue(this.facade.canDiscardCards(PlayerNumber.ONE));
+
+		this.player.resources.brick = 7;
+		this.model.players = new TransportPlayer[1];
+		this.model.players[0] = this.player;
+		this.facade.initializeModel(this.model);
+		/* Should fail because the client player only has 7 resource cards */
+		assertFalse(this.facade.canDiscardCards(PlayerNumber.ONE));
+
+		this.model.turnTracker.status = Status.ROLLING;
+		this.player.resources.brick = 8;
+		this.model.players = new TransportPlayer[1];
+		this.model.players[0] = this.player;
+		this.facade.initializeModel(this.model);
 		/* Should fail because the status is not discarding */
-		assertFalse(facade.canDiscardCards(PlayerNumber.ONE));
+		assertFalse(this.facade.canDiscardCards(PlayerNumber.ONE));
 	}
 
 	@Test
 	public void testCanRollNumber() throws CatanException {
-		model.turnTracker.status = Status.ROLLING;
-		facade.initializeModel(model);
-		/* Should pass because it is the player's turn and the status is
-		 * rolling */
-		assertTrue(facade.canRollNumber(PlayerNumber.ONE));
-		
-		model.turnTracker.currentTurn = PlayerNumber.TWO;
-		facade.initializeModel(model);
-		/* Should fail because it is not the player's turn*/
-		assertFalse(facade.canRollNumber(PlayerNumber.ONE));
+		this.model.turnTracker.status = Status.ROLLING;
+		this.facade.initializeModel(this.model);
+		/*
+		 * Should pass because it is the player's turn and the status is rolling
+		 */
+		assertTrue(this.facade.canRollNumber(PlayerNumber.ONE));
+
+		this.model.turnTracker.currentTurn = PlayerNumber.TWO;
+		this.facade.initializeModel(this.model);
+		/* Should fail because it is not the player's turn */
+		assertFalse(this.facade.canRollNumber(PlayerNumber.ONE));
 	}
 
 	@Test
@@ -179,16 +181,17 @@ public class FacadeTest {
 
 	@Test
 	public void testCanFinishTurn() throws CatanException {
-		model.turnTracker.status = Status.PLAYING;
-		facade.initializeModel(model);
-		/* Should pass because it is the player's turn and the status is
-		 * playing */
-		assertTrue(facade.canFinishTurn(PlayerNumber.ONE));
+		this.model.turnTracker.status = Status.PLAYING;
+		this.facade.initializeModel(this.model);
+		/*
+		 * Should pass because it is the player's turn and the status is playing
+		 */
+		assertTrue(this.facade.canFinishTurn(PlayerNumber.ONE));
 	}
 
 	@Test
 	public void testFinishTurn() throws CatanException {
-		
+
 	}
 
 	@Test
