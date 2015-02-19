21a3607a7ded12f29abf08d7745bb9b2107ae41b
Connor Smith <cjs.smith1993@gmail.com>
2015-02-18 23:21:41 -0700

    fixed formatting of BankTest, commented out unimplemented test

--- test/clientBackend/model/BankTest.java
+++ test/clientBackend/model/BankTest.java
@@ -18,43 +18,32 @@ import shared.definitions.ResourceType;
 
 public class BankTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 	Bank myBank;
 	Bank transBank;
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void testBank() {
-		myBank = new Bank();
-		for(ResourceType type: ResourceType.values()){
-			assertTrue(myBank.getResourceCardCount(type)==25);
+		this.myBank = new Bank();
+		for (ResourceType type : ResourceType.values()) {
+			assertTrue(this.myBank.getResourceCardCount(type) == 25);
 		}
-		for(DevCardType type: DevCardType.values()){
-			switch(type){
+		for (DevCardType type : DevCardType.values()) {
+			switch (type) {
 			case SOLDIER:
-				assertTrue(myBank.getDevelopmentCardCount(type) == 14);
+				assertTrue(this.myBank.getDevelopmentCardCount(type) == 14);
 				break;
 			case MONUMENT:
-				assertTrue(myBank.getDevelopmentCardCount(type) == 5);
+				assertTrue(this.myBank.getDevelopmentCardCount(type) == 5);
 				break;
 			default:
-				assertTrue(myBank.getDevelopmentCardCount(type) == 2);
+				assertTrue(this.myBank.getDevelopmentCardCount(type) == 2);
 				break;
 			}
-			
+
 		}
 	}
 
 	@Test
-
 	public void testBankTransportDeckTransportBank() {
 		//TransportDeck devDeck, TransportBank resDeck
 		TransportDeck devDeck = new TransportDeck();
@@ -69,73 +58,69 @@ public class BankTest {
 		resDeck.sheep = 8;
 		resDeck.wheat = 20;
 		resDeck.wood = 5;
-		transBank = new Bank(devDeck, resDeck);
-		for(ResourceType type: ResourceType.values()){
-			switch(type){
+		this.transBank = new Bank(devDeck, resDeck);
+		for (ResourceType type : ResourceType.values()) {
+			switch (type) {
 			case BRICK:
-				assertTrue(transBank.getResourceCardCount(type)==12);
+				assertTrue(this.transBank.getResourceCardCount(type) == 12);
 				break;
 			case WOOD:
-				assertTrue(transBank.getResourceCardCount(type)==5);
+				assertTrue(this.transBank.getResourceCardCount(type) == 5);
 				break;
 			case WHEAT:
-				assertTrue(transBank.getResourceCardCount(type)==20);
+				assertTrue(this.transBank.getResourceCardCount(type) == 20);
 				break;
 			case ORE:
-				assertTrue(transBank.getResourceCardCount(type)==15);
+				assertTrue(this.transBank.getResourceCardCount(type) == 15);
 				break;
 			case SHEEP:
-				assertTrue(transBank.getResourceCardCount(type)==8);
+				assertTrue(this.transBank.getResourceCardCount(type) == 8);
 				break;
 			default:
-				
 				break;
 			}
-			
 		}
-		for(DevCardType type: DevCardType.values()){
-			switch(type){
+		for (DevCardType type : DevCardType.values()) {
+			switch (type) {
 			case SOLDIER:
-				assertTrue(transBank.getDevelopmentCardCount(type) == 10);
+				assertTrue(this.transBank.getDevelopmentCardCount(type) == 10);
 				break;
 			case MONUMENT:
-				assertTrue(transBank.getDevelopmentCardCount(type) == 3);
+				assertTrue(this.transBank.getDevelopmentCardCount(type) == 3);
 				break;
 			case MONOPOLY:
-				assertTrue(transBank.getDevelopmentCardCount(type) == 1);
+				assertTrue(this.transBank.getDevelopmentCardCount(type) == 1);
 				break;
 			default:
-				assertTrue(transBank.getDevelopmentCardCount(type) == 2);
+				assertTrue(this.transBank.getDevelopmentCardCount(type) == 2);
 				break;
 			}
-			
 		}
-		
 	}
 
-	@Test
-	@Ignore
-	public void testDrawDevelopmentCard() {
-		fail("Not yet implemented");
-	}
+	//	@Test
+	//	@Ignore
+	//	public void testDrawDevelopmentCard() {
+	//		fail("Not yet implemented");
+	//	}
 
 	@Test
 	public void testAddDevelopmentCardCollection() {
-		for(DevCardType type: DevCardType.values())
+		for (DevCardType type : DevCardType.values())
 		{
-			myBank = new Bank();
-			int initSize = myBank.getPlayedCards().size();
+			this.myBank = new Bank();
+			int initSize = this.myBank.getPlayedCards().size();
 			Collection<DevelopmentCard> newCards = new ArrayList<DevelopmentCard>();
 			newCards.add(new DevelopmentCard(type));
-			if(myBank.addDevelopmentCardCollection(type, newCards)){
-				assertTrue("The card had the right type and was added", initSize == myBank.getPlayedCards().size()-1);
+			if (this.myBank.addDevelopmentCardCollection(type, newCards)) {
+				assertTrue("The card had the right type and was added", initSize == this.myBank
+						.getPlayedCards().size() - 1);
 			}
 			else
 			{
-				assertTrue("The card was not added", initSize == myBank.getPlayedCards().size());
+				assertTrue("The card was not added", initSize == this.myBank.getPlayedCards()
+						.size());
 			}
 		}
-		
 	}
-
 }
