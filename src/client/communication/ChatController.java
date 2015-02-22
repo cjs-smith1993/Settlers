b2db289d202d0b1d3188bc607f4e380e915947fb
Connor Smith <cjs.smith1993@gmail.com>
2015-02-21 21:48:42 -0700

    removed unnecessary import in ChatController

--- src/client/communication/ChatController.java
+++ src/client/communication/ChatController.java
@@ -5,13 +5,11 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-import serverCommunication.ServerException;
 import serverCommunication.ServerProxy;
 import shared.definitions.CatanColor;
 import client.base.*;
 import clientBackend.model.Facade;
 import clientBackend.model.Message;
-
 
 /**
  * Chat controller implementation
@@ -19,39 +17,35 @@ import clientBackend.model.Message;
 public class ChatController extends Controller implements IChatController, Observer {
 	ServerProxy proxy = ServerProxy.getInstance();
 	Facade facade = Facade.getInstance();
-	
+
 	public ChatController(IChatView view) {
 		super(view);
-		
+
 		Facade.getInstance().addObserver(this);
 	}
 
 	@Override
 	public IChatView getView() {
-		return (IChatView)super.getView();
+		return (IChatView) super.getView();
 	}
 
 	@Override
 	public void sendMessage(String message) {
-		try {
-			proxy.movesSendChat(facade.getClientPlayer().getInteger(), message);
-		} catch (ServerException e) {
-			e.printStackTrace();
-		}
+		this.proxy.movesSendChat(this.facade.getClientPlayer().getInteger(), message);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		List<Message> messages = facade.getMessages();
-		CatanColor color = facade.getPlayerColor(facade.getClientPlayer());
-		
+		List<Message> messages = this.facade.getMessages();
+		CatanColor color = this.facade.getPlayerColor(this.facade.getClientPlayer());
+
 		List<LogEntry> messageEntries = new ArrayList<>();
-		
+
 		for (int i = 0; i < messages.size(); i++) {
 			LogEntry entry = new LogEntry(color, messages.get(i).getMessage());
 			messageEntries.add(entry);
 		}
-		
-		getView().setEntries(messageEntries);
+
+		this.getView().setEntries(messageEntries);
 	}
 }
