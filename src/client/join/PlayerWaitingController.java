c70c69639ec49e440e773c10a479398c9731e800
Connor Smith <cjs.smith1993@gmail.com>
2015-02-24 12:32:10 -0700

    almost finished PlayerWaitingController

--- src/client/join/PlayerWaitingController.java
+++ src/client/join/PlayerWaitingController.java
@@ -3,8 +3,11 @@ package client.join;
 import java.util.Observable;
 import java.util.Observer;
 
+import shared.definitions.AIType;
 import client.base.*;
+import client.data.PlayerInfo;
 import clientBackend.model.Facade;
+import clientBackend.model.Player;
 
 /**
  * Implementation for the player waiting controller
@@ -27,19 +30,41 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
 
 	@Override
 	public void start() {
+		AIType[] AITypes = this.facade.getAITypes().toArray(new AIType[0]);
+		String[] stringTypes = new String[AITypes.length];
+		for (int i = 0; i < AITypes.length; i++) {
+			stringTypes[i] = AITypes[i].name();
+		}
+		this.getView().setAIChoices(stringTypes);
 		this.getView().showModal();
 	}
 
 	@Override
 	public void addAI() {
-		// TEMPORARY
-		this.getView().closeModal();
+		String type = this.getView().getSelectedAI();
+		boolean success = this.facade.addAI(AIType.valueOf(type));
+		if (success) {
+			int numPlayers = this.facade.getPlayers().size();
+			if (numPlayers >= 4) {
+				this.getView().closeModal();
+			}
+		}
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		Player[] players = this.facade.getPlayers().values().toArray(new Player[0]);
+		PlayerInfo[] playersInfo = new PlayerInfo[players.length];
+		for (int i = 0; i < players.length; i++) {
+			Player player = players[i];
+			PlayerInfo info = new PlayerInfo();
+			info.setId(player.getUser().getUserId());
+			info.setPlayerIndex(player.getNumber().getInteger());
+			info.setName(player.getUser().getName());
+			info.setColor(player.getColor());
+			playersInfo[i] = info;
+		}
 
+		this.getView().setPlayers(playersInfo);
 	}
-
 }
