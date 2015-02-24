613afd92bb15e7590dcc3df638401e26bdddd8de
Connor Smith <cjs.smith1993@gmail.com>
2015-02-24 11:30:33 -0700

    started working on PlayerWaitingController

--- src/client/join/PlayerWaitingController.java
+++ src/client/join/PlayerWaitingController.java
@@ -1,36 +1,45 @@
 package client.join;
 
-import client.base.*;
+import java.util.Observable;
+import java.util.Observer;
 
+import client.base.*;
+import clientBackend.model.Facade;
 
 /**
  * Implementation for the player waiting controller
  */
-public class PlayerWaitingController extends Controller implements IPlayerWaitingController {
+public class PlayerWaitingController extends Controller implements IPlayerWaitingController,
+		Observer {
+
+	private Facade facade;
 
 	public PlayerWaitingController(IPlayerWaitingView view) {
-
 		super(view);
+		this.facade = Facade.getInstance();
+		this.facade.addObserver(this);
 	}
 
 	@Override
 	public IPlayerWaitingView getView() {
-
-		return (IPlayerWaitingView)super.getView();
+		return (IPlayerWaitingView) super.getView();
 	}
 
 	@Override
 	public void start() {
-
-		getView().showModal();
+		this.getView().showModal();
 	}
 
 	@Override
 	public void addAI() {
-
 		// TEMPORARY
-		getView().closeModal();
+		this.getView().closeModal();
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
-
