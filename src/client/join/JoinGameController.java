a54d6cd7beee0ee9def92833af4a5ac044b9c296
Connor Smith <cjs.smith1993@gmail.com>
2015-02-23 17:48:55 -0700

    fixed warning in JoinGameController

--- src/client/join/JoinGameController.java
+++ src/client/join/JoinGameController.java
@@ -14,7 +14,6 @@ import clientBackend.dataTransportObjects.DTOPlayer;
 import clientBackend.model.CatanException;
 import clientBackend.model.Facade;
 
-
 /**
  * Implementation for the join game controller
  */
@@ -25,121 +24,118 @@ public class JoinGameController extends Controller implements IJoinGameControlle
 	private IMessageView messageView;
 	private IAction joinAction;
 	private Facade facade;
-	
+
 	/**
 	 * JoinGameController constructor
-	 * 
-	 * @param view Join game view
-	 * @param newGameView New game view
-	 * @param selectColorView Select color view
-	 * @param messageView Message view (used to display error messages that occur while the user is joining a game)
+	 *
+	 * @param view
+	 *            Join game view
+	 * @param newGameView
+	 *            New game view
+	 * @param selectColorView
+	 *            Select color view
+	 * @param messageView
+	 *            Message view (used to display error messages that occur while
+	 *            the user is joining a game)
 	 */
-	public JoinGameController(IJoinGameView view, INewGameView newGameView, 
-								ISelectColorView selectColorView, IMessageView messageView) {
+	public JoinGameController(IJoinGameView view, INewGameView newGameView,
+			ISelectColorView selectColorView, IMessageView messageView) {
 
 		super(view);
 
-		facade = Facade.getInstance();
-		facade.addObserver(this);
-		setNewGameView(newGameView);
-		setSelectColorView(selectColorView);
-		setMessageView(messageView);
+		this.facade = Facade.getInstance();
+		this.facade.addObserver(this);
+		this.setNewGameView(newGameView);
+		this.setSelectColorView(selectColorView);
+		this.setMessageView(messageView);
 	}
-	
+
 	public IJoinGameView getJoinGameView() {
-		
-		return (IJoinGameView)super.getView();
+		return (IJoinGameView) super.getView();
 	}
-	
+
 	/**
 	 * Returns the action to be executed when the user joins a game
-	 * 
+	 *
 	 * @return The action to be executed when the user joins a game
 	 */
 	public IAction getJoinAction() {
-		
-		return joinAction;
+		return this.joinAction;
 	}
 
 	/**
 	 * Sets the action to be executed when the user joins a game
-	 * 
-	 * @param value The action to be executed when the user joins a game
+	 *
+	 * @param value
+	 *            The action to be executed when the user joins a game
 	 */
-	public void setJoinAction(IAction value) {	
-		
-		joinAction = value;
+	public void setJoinAction(IAction value) {
+		this.joinAction = value;
 	}
-	
+
 	public INewGameView getNewGameView() {
-		
-		return newGameView;
+		return this.newGameView;
 	}
 
 	public void setNewGameView(INewGameView newGameView) {
-		
 		this.newGameView = newGameView;
 	}
-	
+
 	public ISelectColorView getSelectColorView() {
-		
-		return selectColorView;
+		return this.selectColorView;
 	}
+
 	public void setSelectColorView(ISelectColorView selectColorView) {
-		
 		this.selectColorView = selectColorView;
 	}
-	
+
 	public IMessageView getMessageView() {
-		
-		return messageView;
+		return this.messageView;
 	}
+
 	public void setMessageView(IMessageView messageView) {
-		
 		this.messageView = messageView;
 	}
 
 	@Override
 	public void start() {
 		JoinGameView view = (JoinGameView) super.getView();
-		Collection<DTOGame> games = facade.getGamesList();
+		Collection<DTOGame> games = this.facade.getGamesList();
 		PlayerInfo playerInfo = new PlayerInfo();
-		Collection<GameInfo> gameInfo = new ArrayList();
-		
-		for (DTOGame game: games) {
+		Collection<GameInfo> gameInfo = new ArrayList<GameInfo>();
+
+		for (DTOGame game : games) {
 			GameInfo newGame = new GameInfo();
-			
+
 			newGame.setId(game.id);
 			newGame.setTitle(game.title);
-			for (DTOPlayer player: game.players) {
+			for (DTOPlayer player : game.players) {
 				PlayerInfo newPlayer = new PlayerInfo();
-				
+
 				newPlayer.setId(player.id);
 				newPlayer.setName(player.name);
 				newPlayer.setColor(player.color);
-				if(newPlayer.getName() != null) {
+				if (newPlayer.getName() != null) {
 					newGame.addPlayer(newPlayer);
 				}
 			}
 			gameInfo.add(newGame);
 		}
-		
+
 		GameInfo[] information = gameInfo.toArray(new GameInfo[0]);
-		view.setGames( information, playerInfo);
-		
-		getJoinGameView().showModal();
+		view.setGames(information, playerInfo);
+
+		this.getJoinGameView().showModal();
 	}
 
 	@Override
 	public void startCreateNewGame() {
-		
-		getNewGameView().showModal();
+		this.getNewGameView().showModal();
 	}
 
 	@Override
 	public void cancelCreateNewGame() {
-		
-		getNewGameView().closeModal();
+		this.getNewGameView().closeModal();
 	}
 
 	@Override
@@ -148,49 +144,44 @@ public class JoinGameController extends Controller implements IJoinGameControlle
 		boolean randomNumbers;
 		boolean randomPorts;
 		String gameName;
-		
-		randomTiles = newGameView.getRandomlyPlaceHexes();
-		randomNumbers = newGameView.getRandomlyPlaceNumbers();
-		randomPorts = newGameView.getUseRandomPorts();
-		gameName = newGameView.getTitle();
-		
+
+		randomTiles = this.newGameView.getRandomlyPlaceHexes();
+		randomNumbers = this.newGameView.getRandomlyPlaceNumbers();
+		randomPorts = this.newGameView.getUseRandomPorts();
+		gameName = this.newGameView.getTitle();
+
 		try {
-			facade.createGame(randomTiles, randomNumbers, randomPorts, gameName);
-			getNewGameView().closeModal();
-			start();
+			this.facade.createGame(randomTiles, randomNumbers, randomPorts, gameName);
+			this.getNewGameView().closeModal();
+			this.start();
 		} catch (CatanException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
 	@Override
 	public void startJoinGame(GameInfo game) {
-
-		getSelectColorView().showModal();
+		this.getSelectColorView().showModal();
 	}
 
 	@Override
 	public void cancelJoinGame() {
-	
-		getJoinGameView().closeModal();
+		this.getJoinGameView().closeModal();
 	}
 
 	@Override
 	public void joinGame(CatanColor color) {
-		
 		// If join succeeded
-		getSelectColorView().closeModal();
-		getJoinGameView().closeModal();
-		joinAction.execute();
+		this.getSelectColorView().closeModal();
+		this.getJoinGameView().closeModal();
+		this.joinAction.execute();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
-
