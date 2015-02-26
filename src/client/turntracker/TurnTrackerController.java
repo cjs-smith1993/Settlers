29d91a8f668907a91a18f99041c9d42b0f5e225b
Connor Smith <cjs.smith1993@gmail.com>
2015-02-25 17:04:19 -0700

    removed unnecessary imports

--- src/client/turntracker/TurnTrackerController.java
+++ src/client/turntracker/TurnTrackerController.java
@@ -1,57 +1,53 @@
 package client.turntracker;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
-import shared.definitions.PlayerNumber;
 import client.base.*;
 import client.data.PlayerInfo;
 import clientBackend.model.Facade;
-import clientBackend.model.Player;
-
 
 /**
  * Implementation for the turn tracker controller
  */
 public class TurnTrackerController extends Controller implements ITurnTrackerController, Observer {
 	Facade facade = Facade.getInstance();
-	
+
 	public TurnTrackerController(ITurnTrackerView view) {
 		super(view);
-		facade.addObserver(this);
+		this.facade.addObserver(this);
 	}
-	
+
 	@Override
 	public ITurnTrackerView getView() {
-		return (ITurnTrackerView)super.getView();
+		return (ITurnTrackerView) super.getView();
 	}
 
 	@Override
 	public void endTurn() {
-		
+
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		ITurnTrackerView view = getView();
-		List<PlayerInfo> players = facade.getPlayers();
-		
+		ITurnTrackerView view = this.getView();
+		List<PlayerInfo> players = this.facade.getPlayers();
+
 		if (players != null) {
 			for (PlayerInfo playerInfo : players) {
 				if (playerInfo != null) {
-					view.initializePlayer(playerInfo.getPlayerIndex().getInteger(), playerInfo.getName(), playerInfo.getColor());
-					
-					if (playerInfo.getPlayerIndex() == facade.getClientPlayer()) {
+					view.initializePlayer(playerInfo.getPlayerIndex().getInteger(),
+							playerInfo.getName(), playerInfo.getColor());
+
+					if (playerInfo.getPlayerIndex() == this.facade.getClientPlayer()) {
 						view.setLocalPlayerColor(playerInfo.getColor());
 					}
 				}
 			}
-			
-//			PlayerNumber player = facade.getLongestRoadPlayer();
-//			PlayerNumber largestArmyPlayer = facade.getLargestArmyPlayer();
+
+			//			PlayerNumber player = facade.getLongestRoadPlayer();
+			//			PlayerNumber largestArmyPlayer = facade.getLargestArmyPlayer();
 		}
 	}
 }
-
