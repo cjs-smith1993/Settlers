2ae4e2b4ffe1af65616a68a0d99e4f9058c5d86d
Kevin J Reece <kevinjreece@gmail.com>
2015-02-27 23:01:25 -0700

    Implemented DiscardController.increaseAmount(), DiscardController.decreaseAmount(), and DiscardController.initFromModel()

--- src/client/discard/DiscardController.java
+++ src/client/discard/DiscardController.java
@@ -1,5 +1,6 @@
 package client.discard;
 
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -17,13 +18,18 @@ public class DiscardController extends Controller implements IDiscardController,
 	private IWaitView waitView;
 	private Facade facade;
 	
+	private int numberToDiscard;
+	private int currentNumberToDiscard;
+	private Map<ResourceType, Integer> maxResources;
+	private Map<ResourceType, Integer> resourcesToDiscard;
+	
 	/**
 	 * DiscardController constructor
 	 * 
 	 * @param view View displayed to let the user select cards to discard
 	 * @param waitView View displayed to notify the user that they are waiting for other players to discard
 	 */
-	public DiscardController(IDiscardView view, IWaitView waitView) {
+ 	public DiscardController(IDiscardView view, IWaitView waitView) {
 		
 		super(view);
 		
@@ -44,12 +50,34 @@ public class DiscardController extends Controller implements IDiscardController,
 
 	@Override
 	public void increaseAmount(ResourceType resource) {
+		int currentNumber = this.resourcesToDiscard.get(resource);
+		currentNumber++;
+		this.currentNumberToDiscard++;
+		this.resourcesToDiscard.put(resource, currentNumber);
+		this.getDiscardView().setResourceDiscardAmount(resource, currentNumber);
 		
+		if (currentNumber == this.maxResources.get(resource)) {// are we now at the max amount for this resource?
+			this.getDiscardView().setResourceAmountChangeEnabled(resource, false, true);
+		}
+		
+		if (this.currentNumberToDiscard == this.numberToDiscard) {
+			for (ResourceType type : ResourceType.values()) {
+				this.getDiscardView().setResourceAmountChangeEnabled(resource, false, resourcesToDiscard.get(type) > 0);
+			}
+		}
 	}
 
 	@Override
 	public void decreaseAmount(ResourceType resource) {
+		int currentNumber = this.resourcesToDiscard.get(resource);
+		currentNumber--;
+		this.currentNumberToDiscard--;
+		this.resourcesToDiscard.put(resource, currentNumber);
+		this.getDiscardView().setResourceDiscardAmount(resource, currentNumber);
 		
+		if (currentNumber == this.maxResources.get(resource)) {// are we now down to zero for this resource?
+			this.getDiscardView().setResourceAmountChangeEnabled(resource, true, false);
+		}
 	}
 
 	@Override
@@ -57,19 +85,30 @@ public class DiscardController extends Controller implements IDiscardController,
 		
 		getDiscardView().closeModal();
 	}
-	
+
 	public void initFromModel() {
-		if (facade.needsToDiscardCards(facade.getClientPlayer().getPlayerIndex())) {
-			int brick = facade.getResourceCount(ResourceType.BRICK);
-			System.out.println("brick: " + brick);
-			int ore = facade.getResourceCount(ResourceType.ORE);
-			System.out.println("ore: " + ore);
-			int wheat = facade.getResourceCount(ResourceType.WHEAT);
-			System.out.println("wheat: " + wheat);
-			int sheep = facade.getResourceCount(ResourceType.SHEEP);
-			System.out.println("sheep: " + sheep);
-			int wood = facade.getResourceCount(ResourceType.WOOD);
-			System.out.println("wood: " + wood);
+		PlayerNumber clientPlayer = facade.getClientPlayerIndex();
+		if (this.facade.needsToDiscardCards(clientPlayer)) {
+			
+			this.numberToDiscard = this.facade.getNumberToDiscard(clientPlayer);
+			this.currentNumberToDiscard = 0;
+			
+			// Get max number of each resource to discard
+			for (ResourceType type : ResourceType.values()) {
+				int max = this.facade.getResourceCount(type);
+				this.maxResources.put(type, max);
+				this.getDiscardView().setResourceAmountChangeEnabled(type, max > 0, false);
+			}
+			
+			// Set max number of each resource on the view
+			for (ResourceType type : ResourceType.values()) {
+				this.getDiscardView().setResourceMaxAmount(type, maxResources.get(type));
+			}
+			
+			// Initialize discarding list to 0 for each resource
+			for (ResourceType type : ResourceType.values()) {
+				this.resourcesToDiscard.put(type, 0);
+			}
 			
 			this.getDiscardView().showModal();
 			
