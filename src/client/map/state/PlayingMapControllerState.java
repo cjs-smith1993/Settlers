a924dd69051de831e5568882c4f4eb6aac11c1d3
Connor Smith <cjs.smith1993@gmail.com>
2015-02-27 22:39:37 -0700

    started implementing all MapController state classes

--- src/client/map/state/PlayingMapControllerState.java
+++ src/client/map/state/PlayingMapControllerState.java
@@ -1,5 +1,7 @@
 package client.map.state;
 
+import shared.locations.EdgeLocation;
+import shared.locations.VertexLocation;
 import client.map.IMapView;
 import client.map.IRobView;
 import clientBackend.model.Facade;
@@ -8,7 +10,21 @@ public class PlayingMapControllerState extends DefaultMapControllerState {
 
 	public PlayingMapControllerState(Facade facade, IMapView view, IRobView robView) {
 		super(facade, view, robView);
-		// TODO Auto-generated constructor stub
+	}
+
+	public boolean canPlaceRoad(EdgeLocation edge) {
+		System.out.println("can place road at " + edge);
+		return this.facade.canPlaceRoad(this.facade.getClientPlayerIndex(), edge, false);
+	}
+
+	public boolean canPlaceSettlement(VertexLocation vertex) {
+		System.out.println("can place settlement at " + vertex);
+		return this.facade.canPlaceSettlement(this.facade.getClientPlayerIndex(), vertex, false);
+	}
+
+	public boolean canPlaceCity(VertexLocation vertex) {
+		System.out.println("can place city at " + vertex);
+		return this.facade.canPlaceCity(this.facade.getClientPlayerIndex(), vertex);
 	}
 
 }
