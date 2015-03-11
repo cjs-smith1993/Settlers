4e992d53ebe5fa2b6a06a20c6feff260ba69f77f
Kevin J Reece <kevinjreece@gmail.com>
2015-03-11 16:55:28 -0600

    Added the rest of the server commands classes

---
+++ src/server/commands/games/GamesCreateCommand.java
@@ -0,0 +1,5 @@
+package server.commands.games;
+
+public class GamesCreateCommand implements GamesCommand {
+
+}
---
+++ src/server/commands/moves/MovesSendChatCommand.java
@@ -0,0 +1,5 @@
+package server.commands.moves;
+
+public class MovesSendChatCommand implements MovesCommand {
+
+}
