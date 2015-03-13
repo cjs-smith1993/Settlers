fe28de89796e4e86164163069601ce19882f9a40
Connor Smith <cjs.smith1993@gmail.com>
2015-03-12 23:50:46 -0600

    added Javadocs for Command factories

--- src/server/factories/GameCommandFactory.java
+++ src/server/factories/GameCommandFactory.java
@@ -1,5 +1,22 @@
 package server.factories;
 
+import server.commands.game.IGameCommand;
+
+/**
+ * A factory for creating instances of IGameCommand corresponding to a given
+ * request. The factory uses a string to determine the appropriate command type,
+ * then returns an instance of that type.
+ */
 public class GameCommandFactory {
 
+	/**
+	 * Parses the given type and returns an instance of a corresponding command
+	 * 
+	 * @param type
+	 *            A String containing the type of the command
+	 * @return
+	 */
+	public static IGameCommand getCommand(String type) {
+		return null;
+	}
 }
