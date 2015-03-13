df5a2e845dd38bb5f33e5849503f1e5ad7227ab3
Alex Thomas <alexcthomas18@gmail.com>
2015-03-12 23:32:48 -0600

    added java docs to the user command package

--- src/server/commands/user/IUserCommand.java
+++ src/server/commands/user/IUserCommand.java
@@ -1,5 +1,21 @@
 package server.commands.user;
 
-public interface IUserCommand {
+import shared.model.User;
 
+/**
+* Represents the notion of executing the appropriate action for a given server
+* endpoint that begins with /user/
+*/
+
+public interface IUserCommand {
+	
+	/**
+     * Executes the command specified in the provided JSON
+     * 
+     * @param user
+     *            An encapsulation of a user to be verified or added
+     */
+
+	public void execute(User user);
+	
 }
