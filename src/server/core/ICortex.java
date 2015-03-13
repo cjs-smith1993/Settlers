1443a0c6ec6139da4f18fe0dd0a42b306c0f3374
Kevin J Reece <kevinjreece@gmail.com>
2015-03-13 10:24:58 -0600

    Corrected signature of all Moves methods on the ICortex and added javadocs to all Cortex methods as well

--- src/server/core/ICortex.java
+++ src/server/core/ICortex.java
@@ -19,7 +19,7 @@ import client.backend.dataTransportObjects.DTOGame;
 import client.serverCommunication.ServerException;
 
 /**
- * The interface the Commands from use to manipulate user and game data
+ * The interface the Commands use to manipulate user and game data
  * @author kevinjreece
  */
 public interface ICortex {
@@ -28,118 +28,378 @@ public interface ICortex {
 	 * User methods
 	 */
 	
+	/**
+	 * Logs a player into the server
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	public UserCookie userLogin(String username, String password);
 
+	/**
+	 * Registers a new player with the server and logs them in
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	public UserCookie userRegister(String username, String password);
 
 	/*
 	 * Games methods
 	 */
 
+	/**
+	 * If user is authentic, returns a list of current games on the server
+	 * @param user
+	 * @return
+	 */
 	public Collection<DTOGame> gamesList(UserCookie user);
 
+	/**
+	 * If user is authentic, creates a new game on the server
+	 * @param user
+	 * @param randomTiles
+	 * @param randomNumbers
+	 * @param randomPorts
+	 * @param name
+	 * @return
+	 */
 	public DTOGame gamesCreate(UserCookie user, boolean randomTiles, boolean randomNumbers, boolean randomPorts,
 			String name);
 
+	/**
+	 * If user is authentic, adds the user to the specified game 
+	 * @param user
+	 * @param gameId
+	 * @param color
+	 * @return
+	 */
 	public GameCookie gamesJoin(UserCookie user, int gameId, CatanColor color);
 
+	/**
+	 * If user is authentic, saves the appropriate game to the given file
+	 * @param user
+	 * @param gameId
+	 * @param name
+	 * @return
+	 */
 	public boolean gamesSave(UserCookie user, int gameId, String name);
 
+	/**
+	 * If user is authentic, loads a previous game from the given file
+	 * @param user
+	 * @param name
+	 * @return
+	 */
 	public boolean gamesLoad(UserCookie user, String name);
 
 	/*
 	 * Game methods
 	 */
 
+	/**
+	 * If user and game are authentic and the version is not current, 
+	 * returns the updated model for the appropriate game
+	 * @param user
+	 * @param game
+	 * @param version
+	 * @return the model of the corresponding game
+	 * @throws IOException
+	 * @throws ServerException
+	 */
 	public TransportModel gameModel(UserCookie user, GameCookie game, int version) throws IOException, ServerException;//the return type will depend on if we make listener or if we try to send this to a deserializer
 
+	/**
+	 * If user and game are authentic, returns the updated model for the appropriate game
+	 * @param user
+	 * @param game
+	 * @return the model of the corresponding game
+	 * @throws IOException
+	 * @throws ServerException
+	 */
 	public TransportModel gameModel(UserCookie user, GameCookie game) throws IOException, ServerException;
 	
+	/**
+	 * If user and game are authentic, returns the appropriate game to its state right after the Setup Phase
+	 * @param user
+	 * @param game
+	 * @return the updated model of the corresponding game
+	 */
 	public TransportModel gameReset(UserCookie user, GameCookie game);
 
+	/**
+	 * If user and game are authentic, returns a list of every command that has been executed since the game began
+	 * @param user
+	 * @param game
+	 * @return
+	 */
 	public Collection<JsonObject> gameCommands(UserCookie user, GameCookie game);
 
-	public TransportModel gameCommands(UserCookie user, GameCookie game, String commandList);
-
-//	public boolean gameAddAI(AIType AItype);//boolean if they were successfully added
-//
-//	public Collection<AIType> gameListAI(); //returns a json blob that has the types of AI
+	/**
+	 * If user and game are authentic, applies the provided list of commands to the appropriate game
+	 * @param user
+	 * @param game
+	 * @param commandList
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel gameCommands(UserCookie user, GameCookie game, Collection<JsonObject> commandList);
 
 	/*
 	 * Moves methods
 	 */
 
 	/**
-	 * If user and game are authentic, call sendChat() on the appropriate game Facade
+	 * If user and game are authentic, sends the provided chat in the appropriate game
 	 * @param user
 	 * @param game
 	 * @param playerIndex
 	 * @param content
-	 * @return
+	 * @return the updated model of the corresponding game
 	 */
-	public boolean movesSendChat(UserCookie user, GameCookie game, PlayerNumber playerIndex, String content);//returns an updated json of the game there is a message portion of the json blob
+	public TransportModel movesSendChat(
+			UserCookie user, 
+			GameCookie game, 
+			PlayerNumber playerIndex, 
+			String content);
 
-	public boolean movesRollNumber(UserCookie user, GameCookie game, 
+	/**
+	 * If user and game are authentic, rolls the dice for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param number
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesRollNumber(
+			UserCookie user, 
+			GameCookie game, 
 			PlayerNumber playerIndex,
-			int number);//returns updated Json
+			int number);
 
-	public boolean movesRobPlayer(UserCookie user, GameCookie game, 
+	/**
+	 * If user and game are authentic, robs the victim for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param victimIndex
+	 * @param location
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesRobPlayer(
+			UserCookie user, 
+			GameCookie game, 
 			PlayerNumber playerIndex,
 			PlayerNumber victimIndex,
-			HexLocation location);//returns updated json
+			HexLocation location);
 
-	public boolean movesFinishTurn(UserCookie user, GameCookie game, PlayerNumber playerIndex);//returns updated json
+	/**
+	 * If user and game are authentic, finishes the player's turn in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesFinishTurn(
+			UserCookie user, 
+			GameCookie game, 
+			PlayerNumber playerIndex);
 
-	public boolean movesBuyDevCard(UserCookie user, GameCookie game, PlayerNumber playerIndex);//returns updated json
+	/**
+	 * If user and game are authentic, buys a development card for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesBuyDevCard(
+			UserCookie user, 
+			GameCookie game, 
+			PlayerNumber playerIndex);
 
-	public boolean movesYear_of_Plenty(UserCookie user, GameCookie game, 
+	/**
+	 * If user and game are authentic, uses a player's year of plenty development card in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param resource1
+	 * @param resource2
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesYear_of_Plenty(
+			UserCookie user, 
+			GameCookie game, 
 			PlayerNumber playerIndex,
 			ResourceType resource1,
-			ResourceType resource2);//returns updated json
+			ResourceType resource2);
 
-	public boolean movesRoad_Building(UserCookie user, GameCookie game, 
+	/**
+	 * If user and game are authentic, uses a player's road building development card in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param spot1
+	 * @param spot2
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesRoad_Building(
+			UserCookie user, 
+			GameCookie game, 
 			PlayerNumber playerIndex,
 			EdgeLocation spot1,
-			EdgeLocation spot2); //returns updated json
+			EdgeLocation spot2);
 
-	public boolean movesSoldier(
+	/**
+	 * If user and game are authentic, uses a player's soldier development card in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param victimIndex
+	 * @param location
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesSoldier(
+			UserCookie user, 
+			GameCookie game,
 			PlayerNumber playerIndex,
 			PlayerNumber victimIndex,
-			HexLocation location);//returns updated json
+			HexLocation location);
 
-	public boolean movesMonopoly(
+	/**
+	 * If user and game are authentic, uses a player's monopoly development card in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param resource
+	 * @param playerIndex
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesMonopoly(
+			UserCookie user, 
+			GameCookie game,
 			ResourceType resource,
-			PlayerNumber playerIndex);//returns updated json
+			PlayerNumber playerIndex);
 
-	public boolean movesMonument(PlayerNumber playerIndex);//returns updated json
+	/**
+	 * If user and game are authentic, uses a player's monument development card in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesMonument(
+			UserCookie user, 
+			GameCookie game,
+			PlayerNumber playerIndex);
 
-	public boolean movesBuildRoad(
+	/**
+	 * If user and game are authentic, builds a road for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param location
+	 * @param free
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesBuildRoad(
+			UserCookie user, 
+			GameCookie game,
 			PlayerNumber playerIndex,
 			EdgeLocation location,
-			boolean free);//returns updated json
+			boolean free);
 
-	public boolean movesBuildSettlement(
+	/**
+	 * If user and game are authentic, builds a settlement for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param location
+	 * @param free
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesBuildSettlement(
+			UserCookie user, 
+			GameCookie game,
 			PlayerNumber playerIndex,
 			VertexLocation location,
-			boolean free);//returns updated json
+			boolean free);
 
-	public boolean movesBuildCity(
+	/**
+	 * If user and game are authentic, builds a city for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param location
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesBuildCity(
+			UserCookie user, 
+			GameCookie game,
 			PlayerNumber playerIndex,
-			VertexLocation location);//returns updated json
+			VertexLocation location);
 
-	public boolean movesOfferTrade(ResourceInvoice invoice);//returns updated json
+	/**
+	 * If user and game are authentic, offers a trade between players in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param invoice
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesOfferTrade(
+			UserCookie user, 
+			GameCookie game,
+			ResourceInvoice invoice);
 
-	public boolean movesAcceptTrade(
+	/**
+	 * If user and game are authentic, accepts a trade between players in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param willAccept
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesAcceptTrade(
+			UserCookie user, 
+			GameCookie game,
 			PlayerNumber playerIndex,
-			boolean willAccept);//returns updated json
+			boolean willAccept);
 
-	public boolean movesMaritimeTrade(
+	/**
+	 * If user and game are authentic, performs a maritime trade for the player in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param ratio
+	 * @param inputResource
+	 * @param outputResource
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesMaritimeTrade(
+			UserCookie user, 
+			GameCookie game,
 			PlayerNumber playerIndex,
 			int ratio,
 			ResourceType inputResource,
-			ResourceType outputResource);//returns updated json
+			ResourceType outputResource);
 
-	public boolean movesDiscardCards(
-			PlayerNumber playerIndex, int brick, int ore, int sheep, int wheat, int wood);//returns updated json
+	/**
+	 * If user and game are authentic, discards a player's cards in the appropriate game
+	 * @param user
+	 * @param game
+	 * @param playerIndex
+	 * @param brick
+	 * @param ore
+	 * @param sheep
+	 * @param wheat
+	 * @param wood
+	 * @return the updated model of the corresponding game
+	 */
+	public TransportModel movesDiscardCards(
+			UserCookie user, 
+			GameCookie game,
+			PlayerNumber playerIndex, 
+			int brick, 
+			int ore, 
+			int sheep, 
+			int wheat, 
+			int wood);
 
 }
