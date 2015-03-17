607ea19a6e2d4fde770c1353fe7e6005de4298e9
Connor Smith <cjs.smith1993@gmail.com>
2015-03-17 00:17:43 -0600

    changed UserCertificate to match given server implementation

--- src/server/certificates/UserCertificate.java
+++ src/server/certificates/UserCertificate.java
@@ -6,15 +6,21 @@ package server.certificates;
  */
 public class UserCertificate {
 
-	private int userId;
+	private int userID;
 	private String name;
+	private String password;
+
+	public UserCertificate(int userId, String name, String password) {
+		this.userID = userId;
+		this.name = name;
+	}
 
 	public int getUserId() {
-		return this.userId;
+		return this.userID;
 	}
 
 	public void setUserId(int userId) {
-		this.userId = userId;
+		this.userID = userId;
 	}
 
 	public String getName() {
@@ -23,5 +29,13 @@ public class UserCertificate {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
