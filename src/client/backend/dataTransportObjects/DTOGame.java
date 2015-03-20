2c5ffc49ba6b2bad0bfd6171081ddfad31ebb733
Connor Smith <cjs.smith1993@gmail.com>
2015-03-19 21:07:40 -0600

    added constructor to DTOGame

--- src/client/backend/dataTransportObjects/DTOGame.java
+++ src/client/backend/dataTransportObjects/DTOGame.java
@@ -6,4 +6,10 @@ public class DTOGame {
 	public String title;
 	public int id;
 	public ArrayList<DTOPlayer> players;
+
+	public DTOGame(int id, String title, ArrayList<DTOPlayer> players) {
+		this.id = id;
+		this.title = title;
+		this.players = players;
+	}
 }
