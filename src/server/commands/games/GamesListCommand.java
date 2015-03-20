045831532a299c2325faab87b1e963c8b56c9763
Connor Smith <cjs.smith1993@gmail.com>
2015-03-19 21:09:07 -0600

    implemented GamesListCommand

--- src/server/commands/games/GamesListCommand.java
+++ src/server/commands/games/GamesListCommand.java
@@ -1,6 +1,16 @@
 package server.commands.games;
 
+import java.util.Collection;
+
+import client.backend.CatanSerializer;
+import client.backend.dataTransportObjects.DTOGame;
+import client.serverCommunication.ServerException;
 import server.commands.CommandResponse;
+import server.commands.ContentType;
+import server.core.CortexFactory;
+import server.core.ICortex;
+import server.util.StatusCode;
+import shared.model.CatanException;
 
 /**
  * Games command created when the user attempts to get a list of games
@@ -9,7 +19,7 @@ import server.commands.CommandResponse;
 public class GamesListCommand extends AbstractGamesCommand {
 
 	public GamesListCommand(String json) {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	/**
@@ -17,7 +27,30 @@ public class GamesListCommand extends AbstractGamesCommand {
 	 */
 	@Override
 	public CommandResponse execute() {
-		return null;
+		//		[{'title':'DefaultGame','id':0,'players':[{'color':'orange','name':'Sam','id':0},{'color':'blue','name':'Brooke','id':1},{'color':'red','name':'Pete','id':10},{'color':'green','name':'Mark','id':11}]},{'title':'AIGame','id':1,'players':[{'color':'orange','name':'Pete','id':10},{'color':'puce','name':'Steve','id':-2},{'color':'blue','name':'Squall','id':-3},{'color':'yellow','name':'Quinn','id':-4}]},{'title':'EmptyGame','id':2,'players':[{'color':'orange','name':'Sam','id':0},{'color':'blue','name':'Brooke','id':1},{'color':'red','name':'Pete','id':10},{'color':'green','name':'Mark','id':11}]}]
+		CommandResponse response = null;
+		String body;
+		StatusCode status;
+		ContentType contentType;
+
+		try {
+			ICortex cortex = CortexFactory.getInstance().getCortex();
+			Collection<DTOGame> gamesList = cortex.gamesList();
+			body = CatanSerializer.getInstance().serializeObject(gamesList);
+			status = StatusCode.OK;
+			contentType = ContentType.JSON;
+		} catch (CatanException e) {
+			body = e.getMessage();
+			status = StatusCode.INVALID_REQUEST;
+			contentType = ContentType.PLAIN_TEXT;
+		} catch (ServerException e) {
+			body = e.getMessage();
+			status = StatusCode.INTERNAL_ERROR;
+			contentType = ContentType.PLAIN_TEXT;
+		}
+
+		response = new CommandResponse(body, status, contentType);
+		return response;
 	}
 
 }
