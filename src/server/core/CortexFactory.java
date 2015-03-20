f303438406d2eba55a2e490cbe8d80ac5e8b8a50
Connor Smith <cjs.smith1993@gmail.com>
2015-03-19 21:20:44 -0600

    made MockCortex the default for now

--- src/server/core/CortexFactory.java
+++ src/server/core/CortexFactory.java
@@ -1,13 +1,15 @@
 package server.core;
 
 /**
- * Provides the correct ICortex to be used to access the Catan models on the server.
- * Can be programmed to return either the CentralCortex or the MockCortex
+ * Provides the correct ICortex to be used to access the Catan models on the
+ * server. Can be programmed to return either the CentralCortex or the
+ * MockCortex
+ * 
  * @author kevinjreece
  */
 public class CortexFactory {
 
-	public static boolean testingEnabled = false;
+	public static boolean testingEnabled = true;
 	private static CortexFactory instance;
 
 	private CortexFactory() {
@@ -23,14 +25,16 @@ public class CortexFactory {
 
 	/**
 	 * Returns either the MockCortex or the CentralCortex
+	 * 
 	 * @return
 	 */
 	public ICortex getCortex() {
 		return testingEnabled ? MockCortex.getInstance() : CentralCortex.getInstance();
 	}
-	
+
 	/**
 	 * Changes the CortexFactory between testing and production settings
+	 * 
 	 * @param _testingEnabled
 	 */
 	public static void setTestEnabled(boolean _testingEnabled) {
