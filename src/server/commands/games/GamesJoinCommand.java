ea849675e37f54d4069a72abef1a78f9464dbb01
Connor Smith <cjs.smith1993@gmail.com>
2015-03-22 21:23:45 -0600

    fixed GamesJoinCommand

--- src/server/commands/games/GamesJoinCommand.java
+++ src/server/commands/games/GamesJoinCommand.java
@@ -1,15 +1,24 @@
 package server.commands.games;
 
 import client.backend.CatanSerializer;
+import client.serverCommunication.ServerException;
+import server.certificates.GameCertificate;
 import server.commands.CommandResponse;
+import server.commands.ContentType;
+import server.core.CortexFactory;
+import server.core.ICortex;
+import server.util.StatusCode;
 import shared.dataTransportObjects.DTOGamesJoin;
 import shared.definitions.CatanColor;
+import shared.model.CatanException;
 
 /**
  * Games command created when the user attempts to join a game
  *
  */
 public class GamesJoinCommand extends AbstractGamesCommand {
+
+	private static final String SUCCESS_MESSAGE = "Success";
 
 	private int id;
 	private CatanColor color;
@@ -26,7 +35,28 @@ public class GamesJoinCommand extends AbstractGamesCommand {
 	 */
 	@Override
 	public CommandResponse execute() {
-		return null;
+		ICortex cortex = CortexFactory.getInstance().getCortex();
+		CommandResponse response = null;
+		String body;
+		StatusCode status;
+		ContentType contentType;
+		GameCertificate gameCert;
+
+		try {
+			gameCert = cortex.gamesJoin(this.id, this.color);
+			body = SUCCESS_MESSAGE;
+			status = StatusCode.OK;
+			contentType = ContentType.JSON;
+		} catch (CatanException | ServerException e) {
+			gameCert = null;
+			body = e.getMessage();
+			status = StatusCode.INVALID_REQUEST;
+			contentType = ContentType.PLAIN_TEXT;
+		}
+
+		response = new CommandResponse(body, status, contentType);
+		response.setGameCert(gameCert);
+		return response;
 	}
 
 }
