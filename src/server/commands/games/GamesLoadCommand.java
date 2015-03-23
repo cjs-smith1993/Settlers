def354dc14dfbee39edec992181930858b1412dd
Connor Smith <cjs.smith1993@gmail.com>
2015-03-22 22:14:54 -0600

    implemented execute for GamesLoadCommand

--- src/server/commands/games/GamesLoadCommand.java
+++ src/server/commands/games/GamesLoadCommand.java
@@ -1,8 +1,15 @@
 package server.commands.games;
 
 import client.backend.CatanSerializer;
+import client.serverCommunication.ServerException;
+import server.certificates.GameCertificate;
 import server.commands.CommandResponse;
+import server.commands.ContentType;
+import server.core.CortexFactory;
+import server.core.ICortex;
+import server.util.StatusCode;
 import shared.dataTransportObjects.DTOGamesLoad;
+import shared.model.CatanException;
 
 /**
  * Games command created when the user attempts to load a game
@@ -11,7 +18,6 @@ import shared.dataTransportObjects.DTOGamesLoad;
 public class GamesLoadCommand extends AbstractGamesCommand {
 
 	private static final String SUCCESS_MESSAGE = "Success";
-	private static final String FAILURE_MESSAGE = "Failed to load game - no file by that name.";
 
 	private String fileName;
 
@@ -26,7 +32,25 @@ public class GamesLoadCommand extends AbstractGamesCommand {
 	 */
 	@Override
 	public CommandResponse execute() {
-		return null;
+		ICortex cortex = CortexFactory.getInstance().getCortex();
+		CommandResponse response = null;
+		String body;
+		StatusCode status;
+		ContentType contentType;
+
+		try {
+			boolean success = cortex.gamesLoad(this.fileName);
+			body = CommandResponse.getSuccessMessage();
+			status = StatusCode.OK;
+			contentType = ContentType.PLAIN_TEXT;
+		} catch (CatanException | ServerException e) {
+			body = e.getMessage();
+			status = StatusCode.INVALID_REQUEST;
+			contentType = ContentType.PLAIN_TEXT;
+		}
+
+		response = new CommandResponse(body, status, contentType);
+		return response;
 	}
 
 }
