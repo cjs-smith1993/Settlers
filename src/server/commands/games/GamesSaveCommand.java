245b954d6c08b9412c6fadc2518a64af0d03586c
Connor Smith <cjs.smith1993@gmail.com>
2015-03-22 22:17:52 -0600

    implemented execute method for GamesSave

--- src/server/commands/games/GamesSaveCommand.java
+++ src/server/commands/games/GamesSaveCommand.java
@@ -1,17 +1,20 @@
 package server.commands.games;
 
 import client.backend.CatanSerializer;
+import client.serverCommunication.ServerException;
 import server.commands.CommandResponse;
+import server.commands.ContentType;
+import server.core.CortexFactory;
+import server.core.ICortex;
+import server.util.StatusCode;
 import shared.dataTransportObjects.DTOGamesSave;
+import shared.model.CatanException;
 
 /**
  * Games command created when the user attempts to save a game
  *
  */
 public class GamesSaveCommand extends AbstractGamesCommand {
-
-	private static final String SUCCESS_MESSAGE = "Success";
-	private static final String FAILURE_MESSAGE = "Failed to save game";
 
 	private int id;
 	private String fileName;
@@ -28,7 +31,25 @@ public class GamesSaveCommand extends AbstractGamesCommand {
 	 */
 	@Override
 	public CommandResponse execute() {
-		return null;
+		ICortex cortex = CortexFactory.getInstance().getCortex();
+		CommandResponse response = null;
+		String body;
+		StatusCode status;
+		ContentType contentType;
+
+		try {
+			cortex.gamesSave(this.id, this.fileName);
+			body = CommandResponse.getSuccessMessage();
+			status = StatusCode.OK;
+			contentType = ContentType.PLAIN_TEXT;
+		} catch (CatanException | ServerException e) {
+			body = e.getMessage();
+			status = StatusCode.INVALID_REQUEST;
+			contentType = ContentType.PLAIN_TEXT;
+		}
+
+		response = new CommandResponse(body, status, contentType);
+		return response;
 	}
 
 }
