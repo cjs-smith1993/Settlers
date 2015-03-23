a13bdd2356e4643b8238beb2b8cdc796f9c68efa
Kyle Pontius <kbpontius@gmail.com>
2015-03-22 20:00:39 -0600

    Updated TransportLine to accommodate serialization more efficiently.

--- src/shared/transport/TransportLine.java
+++ src/shared/transport/TransportLine.java
@@ -3,4 +3,11 @@ package shared.transport;
 public class TransportLine {
 	public String source;
 	public String message;
+	
+	public TransportLine(){}
+	
+	public TransportLine(String source, String message) {
+		this.source = source;
+		this.message = message;
+	}
 }
