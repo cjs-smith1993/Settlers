086463f9a2a85c5e44a8d3be3c06d94779a514aa
Connor Smith <cjs.smith1993@gmail.com>
2015-03-22 23:29:11 -0600

    implemented execute method for GameReset

--- src/server/commands/game/GameResetCommand.java
+++ src/server/commands/game/GameResetCommand.java
@@ -1,8 +1,14 @@
 package server.commands.game;
 
+import client.backend.CatanSerializer;
 import client.serverCommunication.ServerException;
 import server.commands.CommandResponse;
+import server.commands.ContentType;
+import server.core.CortexFactory;
+import server.core.ICortex;
+import server.util.StatusCode;
 import shared.model.CatanException;
+import shared.transport.TransportModel;
 
 /**
  * Game command created when the user attempts to reset the current game
@@ -11,7 +17,7 @@ import shared.model.CatanException;
 public class GameResetCommand extends AbstractGameCommand {
 
 	public GameResetCommand(String json) {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	/**
@@ -19,7 +25,16 @@ public class GameResetCommand extends AbstractGameCommand {
 	 */
 	@Override
 	public CommandResponse executeInner() throws CatanException, ServerException {
-		return null;
+		ICortex cortex = CortexFactory.getInstance().getCortex();
+		CommandResponse response = null;
+
+		TransportModel model = cortex.gameReset();
+		String body = CatanSerializer.getInstance().serializeObject(model);
+		StatusCode status = StatusCode.OK;
+		ContentType contentType = ContentType.JSON;
+
+		response = new CommandResponse(body, status, contentType);
+		return response;
 	}
 
 }
