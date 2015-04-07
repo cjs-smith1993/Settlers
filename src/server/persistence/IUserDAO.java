ad6cfe833e63534134986e1d4319b0bae25336a0
Alex Thomas <alexcthomas18@gmail.com>
2015-04-06 18:35:22 -0600

    added IUserDAO to the project, also added the needed functions with java docs.

---
+++ src/server/persistence/IUserDAO.java
@@ -0,0 +1,28 @@
+package server.persistence;
+
+import java.util.Collection;
+
+import server.core.ServerUser;
+
+/**
+ * This is the interface to access the user table or collection.
+ * the cortex will call these methods to get users or add them.
+ *
+ */
+public interface IUserDAO {
+	
+	/**
+	 * This will be used to get all the users for our server.
+	 * @return collection of ServerUsers.
+	 */
+	public Collection<ServerUser> getUsers();
+	
+	/**
+	 * This will add the user with the given username and password to the database
+	 * @param username - selected username
+	 * @param password - selected password
+	 * @return the id of the user in the database
+	 */
+	public int createUser(String username, String password);
+
+}
