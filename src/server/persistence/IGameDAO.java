86084f51fcd12dc2ed0a54a6c218ce53504a9c4b
Alex Thomas <alexcthomas18@gmail.com>
2015-04-06 18:29:20 -0600

    added IgamesDAO and the jovadocs for it

---
+++ src/server/persistence/IGameDAO.java
@@ -0,0 +1,34 @@
+package server.persistence;
+
+import java.util.Collection;
+
+import shared.transport.TransportModel;
+
+/**
+ * This is the interface for accessing the games table or collection.
+ * The cortex will call the classes that implement this class to get access the games information.
+ *
+ */
+public interface IGameDAO {
+	
+	/**
+	 * This is the method that will be called to populate the local list of games.
+	 * @return the list of created games and the models of the game.
+	 */
+	public Collection<TransportModel> getGames();
+	
+	/**
+	 * This is for adding a model into the database.
+	 * @param model - model to be added
+	 * @return the database id of the game.
+	 */
+	public int createGame(TransportModel model);
+	
+	/**
+	 * This is to update the database entry specified by the gameID.
+	 * @param gameId - the Id of the game to update
+	 * @param model - the new model to store.
+	 */
+	public void updateGame(int gameId, TransportModel model);
+
+}
